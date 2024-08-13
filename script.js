8c0a0472f01557d49b5cca77544d8797dc27930f
Ebesko <118739260+Ebesko@users.noreply.github.com>
2024-08-13 21:03:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+async function start() {
+    // Fetch courses data from JSON file
+    const response_news = await fetch("news.json");
+    const news = await response_news.json();
+
+    for (let text in news){
+        var firstElement = document.createElement("p");
+        firstElement.innerText = text;
+    }
